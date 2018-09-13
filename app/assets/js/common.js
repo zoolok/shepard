133dd823d787500f292323aec715681475117133
Dmitry <mityasazonow@yandex.ru>
2018-09-13 17:18:27 +0300

    new

--- app/assets/js/common.js
+++ app/assets/js/common.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     /* --------------------------------------------------------
      INPUTS
     ----------------------------------------------------------- */
-    $('.main__form form input').on('keyup focus blur', function() {
+    $('.input-group input').on('keyup focus blur', function() {
         if($(this).val().length > 0){
-            $(this).closest('.main__form form input').addClass('filled');
+            $(this).addClass('filled');
         }
         else{
-            $(this).closest('.main__form form input').removeClass('filled');
+            $(this).removeClass('filled');
         }
     });
 });
